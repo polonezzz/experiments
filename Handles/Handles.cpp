a2d63c27b9b30f4c2d2f7672a3ef3ea69c591613
polonezzz <polonezzz@yandex.ru>
2018-07-25 16:53:58 +0300

    move waterFill to algorithm project

--- Handles/Handles.cpp
+++ Handles/Handles.cpp
@@ -3,47 +3,9 @@
 #include <array>
 #include <iostream>
 
-
-int waterFill(const std::array<int, 9>& land) 
-{
-
-	int leftMax = 0;
-	int rightMax = 0;
-	int left = 0;
-	int right = land.size() - 1;
-	int volume = 0;
-
-	while(left < right) 
-	{
-		if(land[left] > leftMax) 
-		{
-			leftMax = land[left];
-		}
-		
-		if(land[right] > rightMax) 
-		{
-			rightMax = land[right];
-		}
-		
-		if(leftMax >= rightMax) 
-		{
-			volume += rightMax - land[right];
-			right--;
-		}
-		else 
-		{
-			volume += leftMax - land[left];
-			left++;
-		}
-	}
-	return volume;
-}
-
-
 int wmain(int argc, wchar_t* argv[], wchar_t* env[])
 {
-	int vol =  waterFill({2, 5, 1, 4, 6, 4, 7, 7, 6});
-	
+
 	std::wcout << L"------enviroment variables------------\n";
 	wchar_t** pEnvVar = env;
 	while (pEnvVar && *pEnvVar)
